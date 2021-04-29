9e9052e4573a7705da161470b221b1b038bb68ad
lcdunne <lewis.dunne00@gmail.com>
2021-04-29 21:29:43 +0100

    Add config setup

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config.BaseConfig')
+
 
 @app.route('/')
 def index():
+    print(app.config)
     return "index page"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
