93d7cff4080d9c7f08070d9def82e07bc3de3089
lcdunne <lewis.dunne00@gmail.com>
2021-04-30 12:14:01 +0100

    Add email functionality

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
-from flask import Flask
+from flask import Flask, request
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config.from_object('config.BaseConfig')
+mail = Mail(app)
 
 
 @app.route('/')
 def index():
-    print(app.config)
-    return "index page"
+    return "Index page"
+
+
+def _send_email(to, message):
+    msg = Message(message, recipients=[to])
+    try:
+        mail.send(msg)
+        status = True
+    except Exception as e:
+        print(e)
+        status = False
+    return status
+
+
+@app.route('/send')
+def send_email():
+    to = request.args.get('email')
+    print("\nAttempting to send to: ", to)
+    email_status = _send_email(to=to, message="Hello")
+    if email_status:
+        return "Email sent"
+    else:
+        return "Email failed"
+
+# # This route is an example from https://sendgrid.com/blog/sending-emails-from-python-flask-applications-with-twilio-sendgrid/
+# @app.route('/send', methods=['GET', 'POST'])
+# def send_message():
+#     if request.method == 'POST':
+#         recipient = request.form['recipient']
+#         print(recipient)
+#         msg = Message('Twilio SendGrid Test Email', recipients=[recipient])
+#         msg.body = ('Congratulations! You have sent a test email with '
+#                     'Twilio SendGrid!')
+#         msg.html = ('<h1>Twilio SendGrid Test Email</h1>'
+#                     '<p>Congratulations! You have sent a test email with '
+#                     '<b>Twilio SendGrid</b>!</p>')
+#         mail.send(msg)
+#         # flash(f'A test message was sent to {recipient}.')
+#     return "Email page"
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -6,3 +6,16 @@ load_dotenv()
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
+    # SendGrid setup
+    # MAIL_USERNAME = 'apikey'  # os.environ.get('SENDGRID_MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('SENDGRID_MAIL_API_KEY')
+    # MAIL_SERVER = 'smtp.sendgrid.net'
+    # MAIL_USE_TLS = True
+    # MAIL_PORT = 587  # if using SSL, port should be 465
+    # MAIL_DEFAULT_SENDER = os.environ.get('SENDGRID_MAIL_DEFAULT_SENDER')
+
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_USE_TLS = True
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
