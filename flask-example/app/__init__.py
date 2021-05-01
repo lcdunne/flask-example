96a66c3bcd9b4f0ed4de587a176e5eb455a8b572
lcdunne <lewis.dunne00@gmail.com>
2021-05-01 13:50:05 +0100

    Enable database login functionality

--- flask-example/app/__init__.py
+++ flask-example/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import BaseConfig
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
+db = SQLAlchemy(app)
 
 from app import routes
--- flask-example/app/routes.py
+++ flask-example/app/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template, redirect, url_for
-from app import app
+from flask import render_template, redirect, url_for, flash
+from app import app, db
 from app.forms import RegistrationForm, LoginForm
+from app.models import User
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash)
 
 
 @app.route('/')
@@ -13,6 +17,22 @@ def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
+        # Check if the user already exists
+        email = form.email.data
+
+        user = User.query.filter_by(email=email).first()
+
+        if user is None:
+            user = User(
+                username=form.username.data,
+                password_hash=generate_password_hash(form.password.data),
+                email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+
+        else:
+            flash("Already exists. Log in instead.")
+
         return redirect(url_for('login'))
     return render_template('registration.html', title='Register', form=form)
 
@@ -20,10 +40,19 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    n_attempts = 3
 
     if form.validate_on_submit():
-        print("OK")
-        return redirect(url_for('home'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None:
+            to_check = user.password_hash
+            if check_password_hash(to_check, form.password.data):
+                n_attempts = 3
+                return redirect(url_for('home'))
+            else:
+                n_attempts -= 1
+                flash(f'Invalid credentials - {n_attempts} attempts left.')
+                return redirect(url_for('login'))
     return render_template('login.html', title='Log In', form=form)
 
 
